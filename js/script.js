c9305a29e4dc3b9efa338d1ad47fcc1811d651cd
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2022-09-23 01:45:46 -0700

    ハンバーガーメニューの実装

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 $(function() {
   // menuボタンを押した時の挙動
   $('.p-header__button').on('click', function() {
+    $('.c-grid__sidebar').removeClass('is-close');
     $('.c-grid__sidebar').addClass('is-open');
   });
 
   // ×ボタンを押した時の挙動
   $('.p-sidebar__button').on('click', function() {
     $('.c-grid__sidebar').removeClass('is-open');
+    $('.c-grid__sidebar').addClass('is-close');
   });  
 });
 
 $(function() {
   $(window).on('resize', function() {
     $('.c-grid__sidebar').removeClass('is-open');
+    $('.c-grid__sidebar').removeClass('is-close');
   });
 });
