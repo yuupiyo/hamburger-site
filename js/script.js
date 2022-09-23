43fc417a9e96381de7b3c335495c5bb8ec2a3bcb
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2022-09-23 02:27:34 -0700

    サイドバー後ろの黒い部分

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ $(function() {
   $('.p-header__button').on('click', function() {
     $('.c-grid__sidebar').removeClass('is-close');
     $('.c-grid__sidebar').addClass('is-open');
+    $('.c-overlay').addClass('is-open');
   });
 
   // ×ボタンを押した時の挙動
   $('.p-sidebar__button').on('click', function() {
     $('.c-grid__sidebar').removeClass('is-open');
+    $('.c-overlay').removeClass('is-open');
     $('.c-grid__sidebar').addClass('is-close');
   });  
 });
@@ -16,5 +18,6 @@ $(function() {
   $(window).on('resize', function() {
     $('.c-grid__sidebar').removeClass('is-open');
     $('.c-grid__sidebar').removeClass('is-close');
+    $('.c-overlay').removeClass('is-open');
   });
 });
